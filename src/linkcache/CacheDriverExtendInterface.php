44390c73d3936a752465f7574e45d0c71db27adb
DongNan <dongyh@126.com>
2015-10-04 18:52:33 +0800

    完善注释

--- src/linkcache/CacheDriverExtendInterface.php
+++ src/linkcache/CacheDriverExtendInterface.php
@@ -16,15 +16,49 @@ namespace linkcache;
  */
 interface CacheDriverExtendInterface {
 
+    /**
+     * 递增
+     * @param string $key   键名
+     * @param int $step     递增步长
+     * @return boolean      是否成功
+     */
     public function incr($key, $step = 1);
 
+    /**
+     * 浮点数递增
+     * @param string $key   键名
+     * @param float $float  递增步长
+     * @return boolean      是否成功
+     */
     public function incrByFloat($key, $float);
 
+    /**
+     * 递减
+     * @param string $key   键名
+     * @param int $step     递减步长
+     * @return boolean      是否成功
+     */
     public function decr($key, $step = 1);
 
+    /**
+     * 批量设置键值
+     * @param array $sets   键值数组
+     * @return boolean      是否成功
+     */
     public function mSet($sets);
 
+    /**
+     * 批量设置键值(当键名不存在时)
+     * 只有当键值全部设置成功时,才返回true,否则返回false并尝试回滚
+     * @param array $sets   键值数组
+     * @return boolean      是否成功
+     */
     public function mSetNX($sets);
 
+    /**
+     * 批量获取键值
+     * @param array $keys   键名数组
+     * @return array        键值数组
+     */
     public function mGet($keys);
 }
--- src/linkcache/CacheDriverInterface.php
+++ src/linkcache/CacheDriverInterface.php
@@ -16,25 +16,90 @@ namespace linkcache;
  */
 interface CacheDriverInterface {
 
+    /**
+     * 设置键值
+     * @param string $key   键名
+     * @param mixed $value  键值
+     * @param int $time     过期时间,默认为-1,永不过期
+     * @return boolean      是否成功
+     */
     public function set($key, $value, $time = -1);
 
+    /**
+     * 当键名不存在时设置键值
+     * @param string $key   键名
+     * @param mixed $value  键值
+     * @param int $time     过期时间,默认为-1,永不过期
+     * @return boolean      是否成功
+     */
     public function setnx($key, $value, $time = -1);
 
+    /**
+     * 获取键值
+     * @param string $key   键名
+     * @return mixed        键值
+     */
     public function get($key);
 
+    /**
+     * 二次获取键值,在get方法没有获取到值时，调用此方法将有可能获取到
+     * 此方法是为了防止惊群现象发生,配合lock和isLock方法,设置新的缓存
+     * @param string $key   键名
+     * @return mixed        键值
+     */
     public function getTwice($key);
 
+    /**
+     * 对指定键名加锁（此锁并不对键值做修改限制,仅为键名的锁标记）
+     * 此方法可用于防止惊群现象发生,在get方法获取键值无效时,先判断键名是否加锁,
+     * 如果已加锁,则不获取新值;如果未加锁,则获取新值,设置新的缓存
+     * @param string $key   键名
+     * @param int $time     加锁时间
+     * @return boolean      是否成功
+     */
     public function lock($key, $time = 60);
 
+    /**
+     * 对指定键名解锁
+     * 此方法可用于防止惊群现象发生,在get方法获取键值无效时,判断键名是否加锁
+     * @param string $key   键名
+     * @return boolean      是否成功
+     */
     public function isLock($key);
 
+    /**
+     * 删除键值
+     * @param string $key   键名
+     * @return boolean      是否成功
+     */
     public function del($key);
 
+    /**
+     * 是否存在键值
+     * @param string $key   键名
+     * @return boolean      是否存在
+     */
     public function has($key);
 
+    /**
+     * 获取生存剩余时间
+     * @param string $key   键名
+     * @return int          生存剩余时间(单位:秒) -1表示永不过期,-2表示键值不存在
+     */
     public function ttl($key);
 
+    /**
+     * 设置过期时间
+     * @param string $key   键名
+     * @param int $time     过期时间(单位:秒)
+     * @return boolean      是否成功
+     */
     public function expire($key, $time);
 
+    /**
+     * 移除指定键值的过期时间
+     * @param string $key   键名
+     * @return boolean      是否成功
+     */
     public function persist($key);
 }
