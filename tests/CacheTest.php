212b5d18861936288c35d1eb6a8d8df4668bcf52
DongNan <dongyh@126.com>
2015-10-26 23:00:06 +0800

    测试类

--- tests/CacheTest.php
+++ tests/CacheTest.php
@@ -25,6 +25,10 @@ class CacheTest extends PHPUnit_Framework_TestCase
         $this->assertTrue($cache->set('test1', 1));
         $this->assertTrue($cache->set('test2', 2, 3));
         $this->assertTrue($cache->set('testDel', 'del'));
+        $this->assertTrue($cache->set('testExpire', 'expire'));
+        $this->assertTrue($cache->set('testExpireAt', 'expireAt'));
+        $this->assertTrue($cache->set('testPersist', 'persist', 600));
+        $this->assertTrue($cache->set('notNum', 'notNum'));
     }
 
     /**
@@ -72,6 +76,126 @@ class CacheTest extends PHPUnit_Framework_TestCase
     {
         $cache = \linkcache\Cache::getInstance($this->cacheDriver);
         $this->assertTrue($cache->del('test1'));
+    }
+
+    /**
+     * @depends testExpire
+     * @depends testExpireAt
+     */
+    public function testTtl()
+    {
+        $cache = \linkcache\Cache::getInstance($this->cacheDriver);
+        $this->assertEquals(10, $cache->ttl('testExpire'));
+        $this->assertEquals(10, $cache->ttl('testExpireAt'));
+        $this->assertEquals(-1, $cache->ttl('testPersist'));
+        $this->assertEquals(-2, $cache->ttl('testNotExist'));
+    }
+
+    /**
+     * @depends testSet
+     */
+    public function testExpire()
+    {
+        $cache = \linkcache\Cache::getInstance($this->cacheDriver);
+        $this->assertTrue($cache->expire('testExpire', 10));
+    }
+
+    /**
+     * @depends testSet
+     */
+    public function testExpireAt()
+    {
+        $cache = \linkcache\Cache::getInstance($this->cacheDriver);
+        $this->assertTrue($cache->expireAt('testExpireAt', time() + 10));
+    }
+
+    /**
+     * @depends testSet
+     */
+    public function testPersist()
+    {
+        $cache = \linkcache\Cache::getInstance($this->cacheDriver);
+        $this->assertTrue($cache->persist('testPersist'));
+    }
+
+    public function testLock()
+    {
+        $cache = \linkcache\Cache::getInstance($this->cacheDriver);
+        $this->assertTrue($cache->lock('testLock'));
+    }
+
+    /**
+     * @depends testLock
+     */
+    public function testIsLock()
+    {
+        $cache = \linkcache\Cache::getInstance($this->cacheDriver);
+        $this->assertTrue($cache->isLock('testLock'));
+        $this->assertFalse($cache->isLock('testNotLock'));
+    }
+
+    /**
+     * @depends testSet
+     */
+    public function testIncr()
+    {
+        $cache = \linkcache\Cache::getInstance($this->cacheDriver);
+        $this->assertEquals(1, $cache->incr('incr'));
+        $this->assertEquals(3, $cache->incr('incr', 2));
+        $this->assertEquals(2, $cache->incr('incr', -1));
+        $this->assertFalse($cache->incr('incr', 'str'));
+        $this->assertFalse($cache->incr('notNum'));
+    }
+
+    /**
+     * @depends testSet
+     */
+    public function testIncrByFloat()
+    {
+        $cache = \linkcache\Cache::getInstance($this->cacheDriver);
+        $this->assertEquals(1.5, $cache->incrByFloat('incrByFloat', 1.5), '', 0.01);
+        $this->assertEquals(5.501, $cache->incrByFloat('incrByFloat', 3.501), '', 0.0001);
+        $this->assertEquals(3.001, $cache->incrByFloat('incrByFloat', -2.5), '', 0.0001);
+        $this->assertFalse($cache->incrByFloat('incrByFloat', 'str'));
+        $this->assertFalse($cache->incrByFloat('notNum'));
+    }
+
+    /**
+     * @depends testSet
+     */
+    public function testDecr()
+    {
+        $cache = \linkcache\Cache::getInstance($this->cacheDriver);
+        $this->assertEquals(-1, $cache->decr('decr'));
+        $this->assertEquals(-3, $cache->decr('decr', 2));
+        $this->assertEquals(-2, $cache->decr('decr', -1));
+        $this->assertFalse($cache->decr('decr', 'str'));
+        $this->assertFalse($cache->decr('notNum'));
+    }
+
+    public function testMSet()
+    {
+        $cache = \linkcache\Cache::getInstance($this->cacheDriver);
+        $this->assertTrue($cache->mSet(['mset1' => 1, 'mset2' => 2, 'mset3' => 3]));
+    }
+
+    /**
+     * @depends testMSet
+     */
+    public function testMSetNX()
+    {
+        $cache = \linkcache\Cache::getInstance($this->cacheDriver);
+        $this->assertFalse($cache->mSetNX(['mset1' => 1, 'mset2' => 2, 'msetnx3' => 3]));
+        $this->assertFalse($cache->mSetNX(['msetnx1' => 1, 'msetnx2' => 2, 'msetnx3' => 3]));
+    }
+
+    /**
+     * @depends testSet
+     */
+    public function testMGet()
+    {
+        $cache = \linkcache\Cache::getInstance($this->cacheDriver);
+        $this->assertArraySubset(['mset1' => 1, 'mset2' => 2, 'mset3' => 3], $cache->mGet(['mset1', 'mset2', 'mset3']), true);
     }
 
 }
